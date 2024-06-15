814906b50cc42e080103b26192c78b5f6a4d5ca0
Michiel Vlieger <michiel.vlieger@gmail.com>
2024-06-15 15:37:25 +0200

    added simple main menu

---
+++ Scenes/menu_switch_button.gd
@@ -0,0 +1,13 @@
+extends Button
+class_name MenuSwitchButton
+
+@export var to_menu: MenuControl
+signal menu_switch_signal
+
+func _ready():
+	self.add_to_group("menu_switch_buttons")
+	self.pressed.connect(_on_pressed)
+
+func _on_pressed():
+	print(to_menu)
+	menu_switch_signal.emit(to_menu)
---
+++ Scripts/MainMenu/menu_control.gd
@@ -0,0 +1,13 @@
+extends Control
+class_name MenuControl
+#
+#signal menu_switch_signal
+#
+#func _ready():
+	##check for buttons of type MenuSwitchButton
+	##button.pressed.connect(on_menu_switch_button_press.bind(button))
+	#pass
+		#
+#func on_menu_switch_button_press(button: MenuSwitchButton):
+	#print(button.get_meta("to_menu_name"))
+	#menu_switch_signal.emit(self, button.to_menu)
